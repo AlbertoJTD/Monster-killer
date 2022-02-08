5535ac91e5a44da75e6192230c6b9400f5584d5b
AlbertoJTD <albertoJTD@outlook.com>
2022-02-08 15:29:58 -0600

    Added strong attack function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,30 @@
-const ATTACK_VALUE = 10; // Global variable
+// Global variable
+const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
 
-let chosenMaxLife = 5;
+let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damageMonster = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(typeOfAttack) {
+  let maxDamage;
+  if (typeOfAttack === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  }else if (typeOfAttack === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+
+  const damageMonster = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damageMonster;
 
   const damagePlayer = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= damagePlayer
   
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert('You won!, congratulations..')
+    alert('You won!, congratulations...')
   }else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('Monster won!')
   }else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
@@ -23,4 +32,13 @@ function attackHandler() {
   }
 }
 
-attackBtn.addEventListener('click', attackHandler)
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+ attackMonster('STRONG_ATTACK');
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
