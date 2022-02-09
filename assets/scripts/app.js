f555677277c302f384acb0ee5e7ad403f1708a35
AlbertoJTD <albertoJTD@outlook.com>
2022-02-08 19:09:41 -0600

    Added bonus life function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,12 +7,23 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const damagePlayer = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= damagePlayer
+
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(initialPlayerHealth);
+
+    alert('You would be dead but the bonus life save you!');
+  }
   
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('You won!, congratulations...')
